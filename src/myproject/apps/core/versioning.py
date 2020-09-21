bf4d32fc63269c5f57dbbfba520a65b6daa146dc
Duke09 <debopriyo09@outlook.com>
2020-09-21 17:20:56 +0530

    Setup advance

--- src/myproject/apps/core/versioning.py
+++ src/myproject/apps/core/versioning.py
@@ -27,3 +27,43 @@ def get_git_changeset_timestamp(absolute_path):
     changeset_timestamp = timestamp.strftime('%Y%m%d%H%M%S')
     
     return changeset_timestamp
+
+#!/usr/bin/env python
+
+from dateime import datetime
+from subprocess import check_output, CalledProcessError
+import os
+
+def root():
+    ''' returns the absolute path of the repository root '''
+    try:
+        base = check_output(['git', 'rev-parse', '--show-toplevel'])
+    except CalledProcessError:
+        raise IOError('Current working directory is not a git repository')
+    return base.decode('utf-8').strip()
+
+def abspath(relpath):
+    ''' returns the absolute path for a path given relative to the root the
+    git repository
+    '''
+    return os.path.join(root(), relpath)
+
+def add_to_git(file_path):
+    ''' adds a file to git '''
+    try:
+        base = check_output(['git', 'add', 'file_path'])
+    except CalledProcessError:
+        raise IOError('Current woring directory is not a git repository')
+    return base.decode('utf-8').strip()
+
+
+def main():
+    file_path = abspath("myproject/settings/last-update.txt")
+
+    with open(file_path, 'w') as f:
+        f.write(datetime.now().strftime("%Y%m%d%H%M%S"))
+
+        add_to_git(file_path)
+
+if __name__ == '__main__':
+    main()
